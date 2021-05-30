4078a6f79b24ee8485d7fadcf0631190b620eb9b
TheJarki <melendez.jarki@gmail.com>
2021-05-30 20:25:51 +0200

    added main page cards

--- src/containers/main/Main.js
+++ src/containers/main/Main.js
@@ -5,9 +5,17 @@ import MainPageCard from '../../components/mainPageCard/MainPageCard';
 const Main = () => {
     return (
         <>
-            <MainPageCard image='inventory-card.webp' title='Inventory'
-                description='Fill your inventory and find recipes with what you have'
-                buttonName='Add Items' />
+            <div className='main-container'>
+                <MainPageCard image='inventory-card.webp' title='Inventory'
+                    description='Fill your inventory and find recipes with what you have'
+                    buttonName='Add Items' link='/' />
+                <MainPageCard image='find-recipes-card.jpg' title='Discover'
+                    description='Search recipes and add the ingredients to your shopping list'
+                    buttonName='Find recipes' link='/' />
+                <MainPageCard image='food-log-card.jpg' title='Meals tracking'
+                    description='Follow the track of your daily meals, calories and macros'
+                    buttonName='Find recipes' link='/' />
+            </div>
         </>
     );
 }
