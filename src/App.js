43e745e47d6301f0421e971486fa8b5562ab7454
TheJarki <melendez.jarki@gmail.com>
2021-05-30 19:30:04 +0200

    main page card

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.sass';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/header/Header';
+import MainPageCard from './components/mainPageCard/MainPageCard';
 
 const App = () => {
   return (
     <>
-      <Header>
+      <Header />
+      <MainPageCard image='inventory-card.webp' title='Inventory'description='Fill your inventory and find recipes with what you have' buttonName='Add Items'/>
         <BrowserRouter>
           <Switch>
             {/* <Route path="/" component={Main} exact /> */}
             <Redirect to="/" />
           </Switch>
         </BrowserRouter>
-      </Header>
     </>
   );
 }
