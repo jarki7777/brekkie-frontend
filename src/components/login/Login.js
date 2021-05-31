3f3628e6df4a86d86567433e3f0f87b51fafe225
TheJarki <melendez.jarki@gmail.com>
2021-05-31 17:44:43 +0200

    html css first approach

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import './Login.sass';
 
 const Login = () => {
-    const [error, setError] = useState(true)
+    const [error, setError] = useState(true);
+    const [disabled, setDisabled] = useState(true);
 
     const validateLoginFields = (event) => {
 
@@ -13,25 +14,25 @@ const Login = () => {
             <span className='auth-logo'>This is a logo</span>
 
             <div className='toggle-auth'>
-                <div className='active-auth'>Login</div>
-                <div className='inactive-auth'>Signup</div>
+                <div className='active-auth'>Log In</div>
+                <div className='inactive-auth'>Sign Up</div>
             </div>
 
-            {error && <span className='error-msg'>error</span>}
+            {/* {error && <ErrorMsg /> } */}
 
             <form className='auth-form' onSubmit={e => validateLoginFields(e)}>
 
                 <div className='input-field'>
-                    <label for='login-email'>email</label>
+                    <label for='login-email'>E-mail</label>
                     <input className='input-text' type="email" name="email" id='login-email'></input>
                 </div>
 
                 <div className='input-field'>
-                    <label for='login-pw'>password</label>
+                    <label for='login-pw'>Password</label>
                     <input className='input-text' type="email" name="email" id='login-pw'></input>
                 </div>
 
-                <button className="login-btn" name="submit" type="submit">Submit</button>
+                <button className="login-btn" name="submit" type="submit" disabled={disabled}>Submit</button>
 
             </form>
         </div>
