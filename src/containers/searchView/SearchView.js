382533a62f9f7747fcae733d84e0f62da42d9791
TheJarki <melendez.jarki@gmail.com>
2021-06-02 16:34:02 +0200

    search view basic html structure

--- src/containers/searchView/SearchView.js
+++ src/containers/searchView/SearchView.js
@@ -1,16 +1,37 @@
 import './SearchView.sass';
 import RecipeCard from '../../components/recipeCard/RecipeCard';
+import { useState } from 'react';
 
 const SearchView = () => {
+    const [recipes, setRecipes] = useState(null);
+
+    const search = (event) => {
+
+    }
+
     return (
-        <RecipeCard 
-        img='IMG_2443-700x933.jpg'
-        title='3 Ingredient Pomegranate Balsamic Chicken'
-        calories='221'
-        likes='1325'
-        calification='4.3'
-        totalVotes='3135'
-        />
+        <div className='search-view-container'>
+
+            <form className='search-form' onSubmit={(event) => search(event)}>
+                <div className='search-bar'>
+                    <input className='input-text' type='search' name='search' placeholder='Search by keyword'></input>
+                    <button className='login-btn' name='submit' type='submit'>Find recipes</button>
+                </div>
+                <label htmlFor='search-inventory'>Use my inventory ingredients</label>
+                <input className='input-check' type="checkbox" name='search by inventory' id='search-inventory'></input>
+            </form>
+
+            <RecipeCard
+                img='IMG_2443-700x933.jpg'
+                title='3 Ingredient Pomegranate Balsamic Chicken'
+                calories='221'
+                likes='1325'
+                calification='4.3'
+                totalVotes='3135'
+            />
+
+
+        </div>
     );
 }
 
