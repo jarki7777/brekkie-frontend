5c4c226e53a4be02981aaff0ccc8ea9373af7b11
TheJarki <melendez.jarki@gmail.com>
2021-06-02 12:46:36 +0200

    styles first approach

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Main from './containers/main/Main';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import RecipeView from './containers/recipeView/RevipeView';
+import RecipeView from './containers/recipeView/RecipeView';
 
 const App = () => {
   return (
--- src/components/recipeDetail/RecipeDetail.js
+++ src/components/recipeDetail/RecipeDetail.js
@@ -1,41 +1,97 @@
 import './RecipeDetail.sass'
+import { ReactComponent as Liked } from '../../icons/heart-solid.svg';
+import { ReactComponent as Calification } from '../../icons/star-solid.svg';
+
 
 export const RecipeDetail = (props) => {
+    const ingredients = props.ingredients;
+    const instructions = props.instructions;
+    const notes = props.notes;
     return (
         <div className='recipe-container'>
             <div className='recipe-cover'>
-                <img src={process.env.PUBLIC_URL + "/img/" + props.title} ></img>
+                <img className='recipe-img' src={process.env.PUBLIC_URL + "/img/" + props.img} alt='recipe final result'></img>
                 <h1 className='recipe-title'>{props.title}</h1>
-                <span className='recipe-likes'>{props.like}</span>
-                <span className='recipe-likes'>{props.calification}</span>
+                <div className='calories'>{props.calories} Calories per serve</div>
             </div>
+
             <div className='recipe-summary'>
-                <span className='summary-element'>{props.serves}</span>
-                <span className='summary-element'>{props.calories}</span>
-                <span className='summary-element'>{props.prepTime}</span>
-                <span className='summary-element'>{props.cookTime}</span>
-                <span className='summary-element'>{props.TotalTime}</span>
+                <span className='summary-element'>Serves {props.serves}</span>
+                <span className='summary-element'>Prep {props.prepTime}</span>
+                <span className='summary-element'>Cook {props.cookTime}</span>
+                <span className='summary-element'>Total {props.totalTime}</span>
                 <span className='summary-element'>{props.category}</span>
                 <span className='summary-element'>{props.method}</span>
                 <span className='summary-element'>{props.cuisine}</span>
             </div>
+
+            <div className='social-interaction'>
+                <div className='likes-element'>
+                    <Liked />{/* call like endpoint */}
+                    {props.likes}
+                </div>
+                <div className='favs-element'>
+                    <Calification />{/* pop vote system */}
+                    {props.calification}
+                    ({props.totalVotes})
+                </div>
+            </div>
+
             <div className='recipe-description'>{props.description}</div>
-            <div className='recipe-ingredients'>
-                <div className='ingredient-elements'>{props.ingredients}</div>
+
+            <div className='sub-title'>
+                <span>You will need:</span>
             </div>
-            <div className='recipe-notes'>
-                <div className='note-elements'>{props.notes}</div>
+
+            <ul className='recipe-ingredients'>
+                {ingredients && ingredients.map(ingredient => <li
+                    className='recipe-list'
+                    key={ingredients.indexOf(ingredient)}>
+                    {ingredient}
+                </li>)}
+            </ul>
+
+            <div className='sub-title'>
+                <span>Instructions:</span>
             </div>
+
+            <ol className='recipe-instructions'>
+                {instructions && instructions.map(instruction => <li
+                    className='recipe-list'
+                    key={instructions.indexOf(instruction)}>
+                    {instruction}
+                </li>)}
+            </ol>
+
+            {notes &&
+                <div className='sub-title'>
+                    <span>Notes:</span>
+                </div>}
+
+            <ul className='recipe-ingredients'>
+                {notes && notes.map(note => <li
+                    className='recipe-list'
+                    key={notes.indexOf(note)}>
+                    {note}
+                </li>)}
+            </ul>
+
+            <div className='nutritional-title'>
+                <span>Nutritional facts per serving:</span>
+            </div>
+
             <div className='nutritional-info'>
-                <div className='recipe-macronutrient'>{props.fat}</div>
-                <div className='recipe-macronutrient'>{props.saturatedFat}</div>
-                <div className='recipe-macronutrient'>{props.sodium}</div>
-                <div className='recipe-macronutrient'>{props.carbs}</div>
-                <div className='recipe-macronutrient'>{props.fiber}</div>
-                <div className='recipe-macronutrient'>{props.sugar}</div>
-                <div className='recipe-macronutrient'>{props.protein}</div>
+                <ul>
+                    <li className='recipe-list'> Fat: {props.fat}</li>
+                    <li className='recipe-list'>Saturated fat: {props.saturatedFat}</li>
+                    <li className='recipe-list'>Sodium: {props.sodium}</li>
+                    <li className='recipe-list'>Carbs: {props.carbs}</li>
+                    <li className='recipe-list'>Fiber: {props.fiber}</li>
+                    <li className='recipe-list'>Sugar: {props.sugar}</li>
+                    <li className='recipe-list'>Protein: {props.protein}</li>
+                </ul>
             </div>
-        </div>
+        </div >
     );
 }
 
