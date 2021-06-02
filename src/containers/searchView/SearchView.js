966d2447b69782a982698555e73c7a796b5eec5d
TheJarki <melendez.jarki@gmail.com>
2021-06-02 18:03:36 +0200

    search view styles first approach

--- src/containers/searchView/SearchView.js
+++ src/containers/searchView/SearchView.js
@@ -25,11 +25,14 @@ const SearchView = () => {
 
             <form className='search-form' onSubmit={(event) => search(event)}>
                 <div className='search-bar'>
-                    <input className='input-text' type='search' name='search' placeholder='Search by keyword'></input>
-                    <button className='login-btn' name='submit' type='submit'>Find recipes</button>
+                    <input className='input-text search-input' type='search' name='search' placeholder='Search by keyword'></input>
+                    <button className='login-btn search-btn' name='submit' type='submit'>Find recipes</button>
                 </div>
-                <label htmlFor='search-inventory'>Use my inventory ingredients</label>
-                <input className='input-check' type="checkbox" name='search by inventory' id='search-inventory'></input>
+                <div>
+                    <label htmlFor='search-inventory'>Use my inventory ingredients</label>
+                    <input className='input-check' type="checkbox" name='search by inventory' id='search-inventory'></input>
+                </div>
+
             </form>
 
             <RecipeCard
