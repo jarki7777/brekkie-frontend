851280172f5baa7637b5040fc6da53d42ffac6fc
TheJarki <melendez.jarki@gmail.com>
2021-06-03 18:54:09 +0200

    inventory html structure

---
+++ src/components/ingredientsAccordion/IngredientsAccordion.js
@@ -0,0 +1,11 @@
+import './IngredientsAccordion.sass';
+
+const IngredientsAccordion = () => {
+    return (
+        <div>
+            Accordion
+        </div>
+    )
+}
+
+export default IngredientsAccordion;
--- src/components/inventory/Inventory.js
+++ src/components/inventory/Inventory.js
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+import ErrorMsg from '../errorMsg/ErrorMsg';
+import IngredientsAccordion from '../ingredientsAccordion/IngredientsAccordion';
 import './Inventory.sass';
 
 const Inventory = () => {
+    const [error, setError] = useState(`You don't have any ingredients`);
+
     return (
-        <div>Inventory</div>
+        <>
+            <div className='search-view-container'>
+                {error && <ErrorMsg>{error}</ErrorMsg>}
+                <div className='inventory-instructions'>
+                    Select ingredients from the dropdown list or use the input bar to add ingredients individually
+                </div>
+                <form className='search-form inventory-search' /*onSubmit={(event) => search(event)}*/>
+                    <div className='search-bar'>
+                        <input className='input-text search-input' type='search' name='search' placeholder='Add your ingredients'></input>
+                        <button className='login-btn search-btn' name='submit' type='submit'>Add</button>
+                    </div>
+                </form>
+                <IngredientsAccordion />
+                <div className='empty-inventory'>
+                    <button className='login-btn search-btn empty-btn' name='submit' type='submit'>Empty inventory</button>
+                </div>
+            </div>
+        </>
     );
 }
 
