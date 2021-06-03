af97ef75dccd265c4002963183a56bd86a953a3e
TheJarki <melendez.jarki@gmail.com>
2021-06-03 19:44:15 +0200

    ingredients accordion component html and styles first approach

--- src/components/ingredientsAccordion/IngredientsAccordion.js
+++ src/components/ingredientsAccordion/IngredientsAccordion.js
@@ -1,9 +1,60 @@
 import './IngredientsAccordion.sass';
+import { ReactComponent as Add } from '../../icons/plus-solid.svg';
+
+
 
 const IngredientsAccordion = () => {
+
+    const data = [
+        "quinoa",
+        "edamame",
+        "kidney beans",
+        "chickpeas",
+        "red onion",
+        "yellow bell pepper",
+        "red bell pepper",
+        "celery",
+        "green onions",
+        "cilantro",
+        "parsley",
+        "red wine",
+        "olive oil",
+        "salt",
+        "butter",
+        "light brown sugar",
+        "granulated sugar",
+        "white whole wheat flour",
+        "baking soda",
+        "cinnamon",
+        "vanilla extract",
+        "large egg",
+        "small apples",
+        "kale",
+        "blackberries",
+        "raspberries",
+        "blueberries",
+        "strawberries",
+        "avocado",
+        "feta cheese",
+        "balsamic vinaigrette",
+        "lime",
+        "tomato",
+        "coarse sea salt",
+        "garlic powder",
+        "jalapeño"
+    ]
+
     return (
         <div>
-            Accordion
+            <div className='category-wrap'>
+                <span className='categroy-ingredient category-title'>Dairy products<Add /></span>
+            </div>
+            {data.map(ingredient =>
+                <div className='categroy-ingredient'>
+                    <span>{ingredient}</span>
+                    <Add />
+                </div>
+            )}
         </div>
     )
 }
