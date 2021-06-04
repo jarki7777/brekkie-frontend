d8855eca5716d31017d74e42c51e4049f5e4f6ff
TheJarki <melendez.jarki@gmail.com>
2021-06-04 11:47:00 +0200

    remove ingredient from list on click

--- src/components/ownedIngredients/OwnedIngredients.js
+++ src/components/ownedIngredients/OwnedIngredients.js
@@ -1,26 +1,28 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ReactComponent as Minus } from '../../icons/minus-solid.svg';
 import { fetchRemoveFromInventory } from '../../services/fetchInventory';
 
 const OwnedIngredients = (props) => {
     const token = useSelector(state => state.loginState.token);
+    const [ingredient, setIngredient] = useState(true);
 
     const removeIngredient = async () => {
         try {
-            let res = await fetchRemoveFromInventory(token, props.title)
-            console.log(res);
+            await fetchRemoveFromInventory(token, props.title)
+            setIngredient(false);
         } catch (e) {
             console.log(e);
         }
     }
     return (
         <>
-            <div onClick={() => removeIngredient()}>
+            {ingredient && <div onClick={() => removeIngredient()}>
                 <span className='categroy-ingredient'>
                     {props.title}
                     <span><Minus /></span>
                 </span>
-            </div>
+            </div>}
         </>
     )
 }
