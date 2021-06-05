21a4f577641edd728037e2bc4c7a75614eb13788
TheJarki <melendez.jarki@gmail.com>
2021-06-05 20:20:21 +0200

    meal tracker view basic html structure

--- src/containers/mealTracker/MealTracker.js
+++ src/containers/mealTracker/MealTracker.js
@@ -1,9 +1,52 @@
+import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import ErrorMsg from "../../components/errorMsg/ErrorMsg";
 import './MealTracker.sass';
 
+
 const MealTracker = () => {
+    const token = useSelector(state => state.loginState.token);
+    const history = useHistory();
+    const [startDate, setStartDate] = useState(new Date());
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (!token) history.push('/');
+    }, [token]);
+
     return (
-        <div>
-            Tracker
+        <div className='meal-tracker-container'>
+            <h2 className='tracker-title'>Meal Tracker</h2>
+
+            {error && <ErrorMsg>{error}</ErrorMsg>}
+
+            <div className='user-tracker-info'>
+                <div className='tracker-username'>
+                    User
+                </div>
+                <div className='calories-goal'>
+                    1500
+                </div>
+            </div>
+
+            <div className='tracker-date-picker'>
+                <DatePicker
+                    selected={startDate}
+                    dateFormat="MM/yyyy"
+                    showMonthYearPicker
+                    showFullMonthYearPicker
+                    onChange={(date) => {
+                        setStartDate(date);
+                        /*fetchMonth*/
+                    }}
+                />
+            </div>
+
+            {/* <trackerAccordion /> */}
+
         </div>
     );
 }
