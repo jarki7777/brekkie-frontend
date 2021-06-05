69f726261144dcbd89db46d3e80b61582d96ee81
TheJarki <melendez.jarki@gmail.com>
2021-06-05 16:49:16 +0200

    new service add new comment

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ReactComponent as User } from '../../icons/user-solid.svg';
-import { fetchRecipeComments } from '../../services/fetchComments';
+import { fetchRecipeComments, fetchNewComment } from '../../services/fetchComments';
 import UserComment from '../userComment/UserComment';
+import ErrorMsg from '../errorMsg/ErrorMsg';
 import './Comments.sass';
 
 const Comments = (props) => {
     const recipeId = useSelector(state => state.recipeState.id);
     const user = useSelector(state => state.loginState);
+    const [error, setError] = useState(null);
     const [comments, setComments] = useState([]);
     const [limit, setLimit] = useState(10);
     const [page, setPage] = useState(1);
@@ -23,7 +25,18 @@ const Comments = (props) => {
             setComments(res.docs);
             setTotalPages(res.totalPages);
         } catch (e) {
-            console.log(e);
+            setError('Service is currently unavailable, please try again later');
+        }
+    }
+
+    const postComment = async (event) => {
+        event.preventDefault();
+        try {
+            const text = event.target.childNodes[0].textContent;
+            if (text.length > 2) fetchNewComment(recipeId, user.token, text);
+
+        } catch (e) {
+            setError('Service is currently unavailable, please try again later');
         }
     }
 
@@ -32,27 +45,19 @@ const Comments = (props) => {
             <div className='comments-count'>
                 <span>{props.commentsCount} Comments</span>
             </div>
-            <form className='comment-form' /*onSubmit={(event) => postComment(event)}*/>
+            <form className='comment-form' onSubmit={(event) => postComment(event)}>
                 <div className='comment-input'>
                     <div className='comment-input-text'
-                    role='textbox'
-                    name='search'
-                    maxlength='255'
-                    contentEditable
+                        role='textbox'
+                        name='search'
+                        maxlength='255'
+                        contentEditable
                     ></div>
                 </div>
                 <button className='login-btn search-btn post-btn' name='submit' type='submit'>Post</button>
             </form>
-            <div className='user-comment'>
-                <div className='comment-info'>
-                    <div className='username-comment'>
-                        <User />
-                        <div className='username-text'>user</div>
-                    </div>
-                    <div>06-05-2021</div>
-                </div>
-                <div className='comment-text'>this is a comment</div>
-            </div>
+
+            {comments.length === 0 && <ErrorMsg>There are no comments yet, be the first!</ErrorMsg>}
 
             {comments.length !== 0 && comments.map(comment =>
                 <UserComment
--- src/services/fetchComments.js
+++ src/services/fetchComments.js
@@ -11,3 +11,20 @@ export const fetchRecipeComments = async (recipeId, page, limit, token) => {
         console.log(e);
     }
 }
+
+export const fetchNewComment = async (recipeId, token, comment) => {
+    try {
+        const body = {
+            comment: comment
+        }
+        let res = await fetch(`${process.env.REACT_APP_BASE_URL}${process.env.REACT_APP_COMMENTS}${recipeId}`,
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json', 'authorization': `Bearer ${token}` },
+                body: JSON.stringify(body)
+            });
+        return res.status;
+    } catch (e) {
+        console.log(e);
+    }
+}
