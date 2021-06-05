82ffd289457d8dddbf52b7361f677d18a6ae8c66
TheJarki <melendez.jarki@gmail.com>
2021-06-05 21:13:46 +0200

    meal tracker view css styling

--- src/containers/mealTracker/MealTracker.js
+++ src/containers/mealTracker/MealTracker.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import ErrorMsg from "../../components/errorMsg/ErrorMsg";
+import { ReactComponent as User } from '../../icons/user-solid.svg';
+import "react-datepicker/dist/react-datepicker.css";
 import './MealTracker.sass';
 
 
@@ -18,35 +19,36 @@ const MealTracker = () => {
     }, [token]);
 
     return (
-        <div className='meal-tracker-container'>
-            <h2 className='tracker-title'>Meal Tracker</h2>
+        <div className='meal-tracker-view'>
+            <div className='meal-tracker-container'>
+                <h1 className='tracker-title'>Meal Tracker</h1>
 
-            {error && <ErrorMsg>{error}</ErrorMsg>}
+                {error && <ErrorMsg>{error}</ErrorMsg>}
 
-            <div className='user-tracker-info'>
-                <div className='tracker-username'>
-                    User
+                <div className='user-tracker-info'>
+                    <div className='tracker-username'><User />User</div>
+                    <div className='calories-goal'>
+                        <div>Calories Goal</div>
+                        <div>1500</div>
+                    </div>
                 </div>
-                <div className='calories-goal'>
-                    1500
+
+                <div className='tracker-date-picker'>
+                    <div className='tracker-calendar-text'>Select the month you want to track</div>
+                    <DatePicker
+                        selected={startDate}
+                        dateFormat="MM/yyyy"
+                        showMonthYearPicker
+                        onChange={(date) => {
+                            setStartDate(date);
+                            /*fetchMonth*/
+                        }}
+                    />
                 </div>
+
+                {/* <trackerAccordion /> */}
+
             </div>
-
-            <div className='tracker-date-picker'>
-                <DatePicker
-                    selected={startDate}
-                    dateFormat="MM/yyyy"
-                    showMonthYearPicker
-                    showFullMonthYearPicker
-                    onChange={(date) => {
-                        setStartDate(date);
-                        /*fetchMonth*/
-                    }}
-                />
-            </div>
-
-            {/* <trackerAccordion /> */}
-
         </div>
     );
 }
