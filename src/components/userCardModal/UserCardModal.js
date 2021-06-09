19f725f7376a1dbe97f53c745e376f02314e0be6
TheJarki <melendez.jarki@gmail.com>
2021-06-09 17:17:08 +0200

    modal css html structure

--- src/components/userCardModal/UserCardModal.js
+++ src/components/userCardModal/UserCardModal.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { ReactComponent as Close } from '../../icons/times-solid.svg';
 import reactDom from "react-dom";
 import './UserCardModal.sass';
+import { fetchUpdateUserCalories } from "../../services/fetchUser";
 
 const UserCardModal = (props) => {
 
@@ -9,13 +10,25 @@ const UserCardModal = (props) => {
         if (!props.open) return null;
     }, [props.open]);
 
+    const fetchUpdateUserCalories = (event) => {
+
+    }
 
     return reactDom.createPortal(
         <>
             <div className='modal-overlay'></div>
             <div className='admin-modal-container'>
                 <div className='icon-button close-icon' onClick={props.onClose}><Close /></div>
-                <input type='text' value={props.user.email}></input>
+                <form className='update-user-form' onSubmit={(event) => fetchUpdateUserCalories(event)}>
+
+                    <input className='input-text update-input' type='text' defaultValue={props.user.email}></input>
+                    <input className='input-text update-input' type='text' defaultValue={props.user.username}></input>
+                    <input className='input-text update-input' type='text' defaultValue={props.user.role}></input>
+                    <div className='admin-modal-btn'>
+                        <button className='admin-btn users-btn' name='send' type='submit'>update</button>
+                        <button className='admin-btn users-btn' name='send' type='submit'>delete</button>
+                    </div>
+                </form>
 
 
             </div>
